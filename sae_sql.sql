93058abb0239e76cacc1ddbf1445343be95ebaa2
JulesCarmille <jules.carmillepro@gmail.com>
2026-01-22 16:03:45 +0100

    remise des pendule a l'heure

--- sae_sql.sql
+++ sae_sql.sql
@@ -1,26 +1,23 @@
-DROP TABLE IF EXISTS Type;
-DROP TABLE IF EXISTS Velo;
+use S2SAE04
+DROP TABLE IF EXISTS utilisateur;
 
-CREATE TABLE Type(
-    idType INT AUTO_INCREMENT PRIMARY KEY,
-    libelleType VARCHAR(50)
+CREATE TABLE utilisateur (
+    id_utilisateur INT PRIMARY KEY AUTO_INCREMENT,
+    login VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    est_actif BOOLEAN NOT NULL DEFAULT TRUE,
+    nom VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE
 );
 
-CREATE TABLE Velo
-(
-    idVelo INT AUTO_INCREMENT PRIMARY KEY,
-    libelleVelo VARCHAR(50),
-    idType INT,
-    prixVelo INT,
-    tailleVelo INT,
-    couleurVelo INT,
-    FOREIGN KEY (idType) REFERENCES Type(idType)
-);
-
-INSERT INTO Type (libelleType) VALUES ('Downhill');
-INSERT INTO Type (libelleType) VALUES ('Road');
-INSERT INTO Type (libelleType) VALUES ('BMX');
-INSERT INTO Type (libelleType) VALUES ('Trial');
-
-
-INSERT INTO Velo VALUES (NULL, )
+INSERT INTO utilisateur(id_utilisateur,login,email,password,role,nom,est_actif) VALUES
+(1,'admin','admin@admin.fr',
+    'pbkdf2:sha256:1000000$eQDrpqICHZ9eaRTn$446552ca50b5b3c248db2dde6deac950711c03c5d4863fe2bd9cef31d5f11988',
+    'ROLE_admin','admin','1'),
+(2,'client','client@client.fr',
+    'pbkdf2:sha256:1000000$jTcSUnFLWqDqGBJz$bf570532ed29dc8e3836245f37553be6bfea24d19dfb13145d33ab667c09b349',
+    'ROLE_client','client','1'),
+(3,'client2','client2@client2.fr',
+    'pbkdf2:sha256:1000000$qDAkJlUehmaARP1S$39044e949f63765b785007523adcde3d2ad9c2283d71e3ce5ffe58cbf8d86080',
+    'ROLE_client','client2','1');
