e4dfb1b86f2c06061463ff4f15aab0a39ea08856
JulesCarmille <jules.carmillepro@gmail.com>
2026-01-22 15:30:20 +0100

    Tables et Insertion pour Type

--- sae_sql.sql
+++ sae_sql.sql
@@ -1,1 +1,25 @@
+DROP TABLE IF EXISTS Type;
+DROP TABLE IF EXISTS Velo;
+
+CREATE TABLE Type(
+    idType INT AUTO_INCREMENT PRIMARY KEY,
+    libelleType VARCHAR(50)
+);
+
+CREATE TABLE Velo
+(
+    idVelo INT AUTO_INCREMENT PRIMARY KEY,
+    libelleVelo VARCHAR(50),
+    idType INT,
+    prixVelo INT,
+    tailleVelo INT,
+    couleurVelo INT,
+    FOREIGN KEY (idType) REFERENCES Type(idType)
+);
+
+INSERT INTO Type (libelleType) VALUES ('Downhill');
+INSERT INTO Type (libelleType) VALUES ('Road');
+INSERT INTO Type (libelleType) VALUES ('BMX');
+INSERT INTO Type (libelleType) VALUES ('Trial');
+
 
