bab194e7d2e286301d9e0a91dc65d4da8bdba2b7
Emile <emile.cap@icloud.com>
2026-01-30 09:58:40 +0100

    maaaaaaa maaaaa

--- connexion_db.py
+++ connexion_db.py
@@ -4,21 +4,27 @@ import pymysql.cursors
 
 from flask import g
 import mysql
-import os                                 # à ajouter
-from dotenv import load_dotenv            # à ajouter
-load_dotenv()                             # à ajouter
+
+from dotenv import load_dotenv  # à ajouter
+
+load_dotenv()  # à ajouter
+
+
+import os
 
 def get_db():
     if 'db' not in g:
-        g.db =  pymysql.connect(
-            host=os.environ.get(mysql.railway.internal),                # à modifier
-            user=os.environ.get(root),               # à modifier
-            password=os.environ.get("PASSWORD"),        # à modifier
-            database=os.environ.get("DATABASE"),        # à modifier
-            charset='utf8mb4',
+        g.db = pymysql.connect(
+            host=os.environ.get("MYSQLHOST"),
+            user=os.environ.get("MYSQLUSER"),
+            password=os.environ.get("MYSQLPASSWORD"),
+            database=os.environ.get("MYSQLDATABASE"),
+            port=int(os.environ.get("MYSQLPORT")),
+            charset="utf8mb4",
             cursorclass=pymysql.cursors.DictCursor
         )
     return g.db
+
 
 def activate_db_options(db):
     cursor = db.cursor()
@@ -28,19 +34,20 @@ def activate_db_options(db):
     if result:
         modes = result['Value'].split(',')
         if 'ONLY_FULL_GROUP_BY' not in modes:
-            print('MYSQL : il manque le mode ONLY_FULL_GROUP_BY')   # mettre en commentaire
+            print('MYSQL : il manque le mode ONLY_FULL_GROUP_BY')  # mettre en commentaire
             cursor.execute("SET sql_mode=(SELECT CONCAT(@@sql_mode, ',ONLY_FULL_GROUP_BY'))")
             db.commit()
         else:
-            print('MYSQL : mode ONLY_FULL_GROUP_BY  ok')   # mettre en commentaire
+            print('MYSQL : mode ONLY_FULL_GROUP_BY  ok')  # mettre en commentaire
     # Vérifier et activer l'option lower_case_table_names si nécessaire
     cursor.execute("SHOW VARIABLES LIKE 'lower_case_table_names'")
     result = cursor.fetchone()
     if result:
         if result['Value'] != '0':
-            print('MYSQL : valeur de la variable globale lower_case_table_names differente de 0')   # mettre en commentaire
+            print(
+                'MYSQL : valeur de la variable globale lower_case_table_names differente de 0')  # mettre en commentaire
             cursor.execute("SET GLOBAL lower_case_table_names = 0")
             db.commit()
-        else :
-            print('MYSQL : variable globale lower_case_table_names=0  ok')    # mettre en commentaire
+        else:
+            print('MYSQL : variable globale lower_case_table_names=0  ok')  # mettre en commentaire
     cursor.close()
