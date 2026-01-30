f70a5ae57393baf5038496153fcaf0643c8bf08d
JulesCarmille <jules.carmillepro@gmail.com>
2026-01-30 10:51:10 +0100

    dot env associer pour lhébergement

--- connexion_db.py
+++ connexion_db.py
@@ -3,28 +3,29 @@ from flask import Flask, request, render_template, redirect, url_for, abort, fla
 import pymysql.cursors
 
 from flask import g
-import mysql
 
-from dotenv import load_dotenv  # à ajouter
+import os                                 # à ajouter
+from dotenv import load_dotenv            # à ajouter
 
-load_dotenv()  # à ajouter
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+dotenv_path = os.path.join(BASE_DIR, "bddPerso.env")
 
+load_dotenv(dotenv_path)
 
-import os
-
-
-import mysql.connector
+print("Fichier .env chargé depuis :", dotenv_path)
+print("LOGIN =", os.getenv("LOGIN"))
 
 def get_db():
-    db = mysql.connector.connect(
-        host=os.environ["mysql.railway.internal"],
-        user=os.environ["root"],
-        password=os.environ["OTyUUJRjyHMcXtDAhcCdXvdNUPTMOAcR"],
-        database=os.environ["railway"],
-        port=int(os.environ["3306"])
-    )
-    return db
-
+    if 'db' not in g:
+        g.db =  pymysql.connect(
+            host=os.environ.get("HOST"),                # à modifier
+            user=os.environ.get("LOGIN"),               # à modifier
+            password=os.environ.get("PASSWORD"),        # à modifier
+            database=os.environ.get("DATABASE"),        # à modifier
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor
+        )
+    return g.db
 
 def activate_db_options(db):
     cursor = db.cursor()
@@ -34,20 +35,19 @@ def activate_db_options(db):
     if result:
         modes = result['Value'].split(',')
         if 'ONLY_FULL_GROUP_BY' not in modes:
-            print('MYSQL : il manque le mode ONLY_FULL_GROUP_BY')  # mettre en commentaire
+            print('MYSQL : il manque le mode ONLY_FULL_GROUP_BY')   # mettre en commentaire
             cursor.execute("SET sql_mode=(SELECT CONCAT(@@sql_mode, ',ONLY_FULL_GROUP_BY'))")
             db.commit()
         else:
-            print('MYSQL : mode ONLY_FULL_GROUP_BY  ok')  # mettre en commentaire
+            print('MYSQL : mode ONLY_FULL_GROUP_BY  ok')   # mettre en commentaire
     # Vérifier et activer l'option lower_case_table_names si nécessaire
     cursor.execute("SHOW VARIABLES LIKE 'lower_case_table_names'")
     result = cursor.fetchone()
     if result:
         if result['Value'] != '0':
-            print(
-                'MYSQL : valeur de la variable globale lower_case_table_names differente de 0')  # mettre en commentaire
+            print('MYSQL : valeur de la variable globale lower_case_table_names differente de 0')   # mettre en commentaire
             cursor.execute("SET GLOBAL lower_case_table_names = 0")
             db.commit()
-        else:
-            print('MYSQL : variable globale lower_case_table_names=0  ok')  # mettre en commentaire
+        else :
+            print('MYSQL : variable globale lower_case_table_names=0  ok')    # mettre en commentaire
     cursor.close()
